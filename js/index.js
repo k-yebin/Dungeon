fbba5c66785e63f0004541b9e8c096453b8f8338
unknown <mss3662352@gmail.com>
2025-04-07 23:59:19 +0900

    firstCommit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".video_load").hide().fadeIn(500); // 모든 이미지를 숨긴 후 1초 동안 페이드인
+
+});
+
+$(function(){
+    $(".animate").scrolla({
+        mobile : true,
+        once : false
+    })
+})
+
+$(document).ready(function () {
+    $(".video_load").hide().fadeIn(1900); // 모든 이미지를 숨긴 후 1.9초 동안 페이드인
+});
+
+$(function() {
+    $(".animate").scrolla({
+        mobile: true,
+        once: false
+    });
+});
+
+
